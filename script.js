712d331a036d9ecd9429cd3061fb66ede91e5699
R1nge <r1ngegamedev@gmail.com>
2024-01-09 23:35:18 +0300

    Parse json

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-var createModButton = document.querySelector('.create-mod-button');
-createModButton.addEventListener('click', function() {  
-  var input = document.createElement('input');
-  input.type = 'file';
-  
-  input.onchange = e => { 
-     var file = e.target.files[0]; 
-  
-     var reader = new FileReader();
-     reader.readAsText(file,'UTF-8');
-  
-     reader.onload = readerEvent => {
-        var content = readerEvent.target.result;
-        console.log( content );
-     }
-  }
-  
-  input.click();
+const createModButton = document.querySelector('.create-mod-button');
+
+createModButton.addEventListener('click', function () {
+    const input = document.createElement('input');
+    input.type = 'file';
+
+    input.onchange = e => {
+        const file = e.target.files[0];
+
+        const reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+
+        reader.onload = readerEvent => {
+            const json = readerEvent.target.result;
+            const gameConfig = JSON.parse(json);
+            console.log(gameConfig);
+        }
+    }
+
+    input.click();
 });
+
+
+const gameConfig = {
+    ModName: "",
+    ModIconPath: "",
+    ContainerImagePath: "",
+    SuikaSkinsImagesPaths: [],
+    SuikaIconsPaths: [],
+    SuikaAudios: [], //AudioData
+    SuikaDropChances: [],
+    TimeBeforeTimerTrigger: 0,
+    TimerStartTime: 0,
+    InGameBackgroundPath: "",
+    LoadingScreenBackgroundPath: "",
+    LoadingScreenIconPath: "",
+    PlayerSkinPath: "",
+    MergeSoundsAudios: [], //AudioData
+    MainMenuBackgroundPath: ""
+}
+
+const audioData = {
+    Path: "",
+    Volume: 0
+}
