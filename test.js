bcdf5e9a8736ae56b0669c87f8908e20eef56f2d
R1nge <r1ngegamedev@gmail.com>
2024-01-11 20:08:19 +0300

    Rewriting in TS

--- test.js
+++ test.js
@@ -1,2 +1,65 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var gameConfig = {
+    ModName: "Default",
+    ModIconPath: "gura.png",
+    ContainerImagePath: "container.png",
+    SuikaSkinsImagesPaths: [
+        "yagoo.png",
+        "sana.png",
+        "ollie.png",
+        "aqua.png",
+        "ayame.png",
+        "fubuki.png",
+        "gura.png",
+        "hakos.png",
+        "mio.png",
+        "kobo.png",
+        "koyori.png",
+        "towa.png"
+    ],
+    SuikaIconsPaths: [
+        "yagoo.png",
+        "sana.png",
+        "ollie.png",
+        "aqua.png",
+        "ayame.png",
+        "fubuki.png",
+        "gura.png",
+        "hakos.png",
+        "mio.png",
+        "kobo.png",
+        "koyori.png",
+        "towa.png"
+    ],
+    SuikaAudios: [
+        { path: "silence.mp3", volume: 1 },
+        { path: "silence.mp3", volume: 1 },
+        { path: "song1.mp3", volume: 0.1 },
+        { path: "song1.mp3", volume: 0.1 },
+        { path: "song1.mp3", volume: 0.1 },
+        { path: "song1.mp3", volume: 0.1 },
+        { path: "silence.mp3", volume: 1 },
+        { path: "silence.mp3", volume: 1 },
+        { path: "silence.mp3", volume: 1 },
+        { path: "silence.mp3", volume: 1 },
+        { path: "silence.mp3", volume: 1 },
+        { path: "silence.mp3", volume: 1 }
+    ],
+    SuikaDropChances: [
+        0.1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+};
+var modTitleElement = document.querySelector('#mod-title');
 var message = "Hello, World!";
-console.log(message);
+console.log(gameConfig.ModName);
--- test.ts
+++ test.ts
@@ -1,2 +1,83 @@
-﻿let message: string = "Hello, World!";
-console.log(message);
+﻿import {downloadZip} from "./client-zip.js";
+
+type AudioData = {
+    path: string,
+    volume: number
+}
+
+type GameConfig = {
+    ModName: string,
+    ModIconPath: string,
+    ContainerImagePath: string,
+    SuikaSkinsImagesPaths: string[],
+    SuikaIconsPaths: string[],
+    SuikaAudios: AudioData[],
+    SuikaDropChances: number[],
+}
+
+
+const gameConfig: GameConfig = {
+    ModName: "Default",
+    ModIconPath: "gura.png",
+    ContainerImagePath: "container.png",
+    SuikaSkinsImagesPaths: [
+        "yagoo.png",
+        "sana.png",
+        "ollie.png",
+        "aqua.png",
+        "ayame.png",
+        "fubuki.png",
+        "gura.png",
+        "hakos.png",
+        "mio.png",
+        "kobo.png",
+        "koyori.png",
+        "towa.png"
+    ],
+    SuikaIconsPaths: [
+        "yagoo.png",
+        "sana.png",
+        "ollie.png",
+        "aqua.png",
+        "ayame.png",
+        "fubuki.png",
+        "gura.png",
+        "hakos.png",
+        "mio.png",
+        "kobo.png",
+        "koyori.png",
+        "towa.png"
+    ],
+    SuikaAudios: [
+        {path: "silence.mp3", volume: 1},
+        {path: "silence.mp3", volume: 1},
+        {path: "song1.mp3", volume: 0.1},
+        {path: "song1.mp3", volume: 0.1},
+        {path: "song1.mp3", volume: 0.1},
+        {path: "song1.mp3", volume: 0.1},
+        {path: "silence.mp3", volume: 1},
+        {path: "silence.mp3", volume: 1},
+        {path: "silence.mp3", volume: 1},
+        {path: "silence.mp3", volume: 1},
+        {path: "silence.mp3", volume: 1},
+        {path: "silence.mp3", volume: 1}
+    ],
+    SuikaDropChances: [
+        0.1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+}
+
+const modTitleElement = document.querySelector('#mod-title');
+
+let message: string = "Hello, World!";
+console.log(gameConfig.ModName);
